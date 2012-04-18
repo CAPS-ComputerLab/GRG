456e61c497727444dbe038c2c7a03466770d24ff
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2012-04-18 14:38:14 -0400

    class commit test

---
+++ src/org/grassius/grg/GRGImporter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.grassius.grg;
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class GRGImporter {
+    
+}
